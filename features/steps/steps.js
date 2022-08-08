712aebc7c8b982408038cea350944e1031cc3acf
Fabian Guidobaldi <fabian.guidobaldi@mercadolibre.com>
2022-08-08 11:03:05 -0300

    Add dependencies and test attachments

--- features/steps/steps.js
+++ features/steps/steps.js
@@ -8,8 +8,8 @@ Given('a list of {int} users', function (int) {
     while (i <= int) {
         let user = {
             id: i,
-            name: faker.name.firstName,
-            vehicle: faker.vehicle.vehicle
+            name: faker.name.firstName(),
+            vehicle: faker.vehicle.vehicle()
         }
 
         this.users.push(user)
@@ -34,6 +34,10 @@ Then('I should receive user data', function () {
     expect(this.data).toBeTruthy()
 });
 
-Then("I shouldn't receive user data", function () {
-    expect(this.data).toBeFalsy()
+Then('I attach some test info', function () {
+    const attachmentsStep = this.allure.startStep("Attachments");
+    const userInfoStep = this.allure.startStep("User Info")
+    this.attach(JSON.stringify(this.data), 'application/json');
+    userInfoStep.endStep();
+    attachmentsStep.endStep();
 });
--- features/test.feature
+++ features/test.feature
@@ -4,14 +4,13 @@ Feature: This is a feature
   Background:
     Given a list of 10 users
 
-  @TEST
   Scenario: Get data from valid user
     Given user ID 10
     When I try to load data from user
     Then I should receive user data
 
-  @TEST
-  Scenario: Get data from invalid user
-    Given user ID 1500
+  Scenario: Get data from another valid user and save attachments
+    Given user ID 9
     When I try to load data from user
-    Then I shouldn't receive user data
+    Then I should receive user data
+    * I attach some test info
