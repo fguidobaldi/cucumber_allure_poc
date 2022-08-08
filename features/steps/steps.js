2a2d3e65ce3b101eee69031de59043037dc38669
Fabian Guidobaldi <fabian.guidobaldi@mercadolibre.com>
2022-08-08 11:14:13 -0300

    Update references to @cucumber package

--- features/steps/steps.js
+++ features/steps/steps.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require('cucumber');
+const { Given, When, Then } = require('@cucumber/cucumber');
 const { faker } = require('@faker-js/faker')
 const expect = require('expect');
 
